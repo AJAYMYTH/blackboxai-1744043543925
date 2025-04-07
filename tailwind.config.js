017537a85c7e7868d0a367724ef446126d998be1
AJAYMYTH <javaliajayakumar8574@gmail.com>
2025-04-07 22:02:16 +0530

    Add tailwind.config.js 1744043563141

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './*.html',
+    './**/*.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'emerald': {
+          500: '#10b981',
+          600: '#059669',
+          700: '#047857'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
